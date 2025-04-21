f2fd4fa847c8264c9f15b76e5cbd3cae94d272b5
PeddolluDeva <peddolludeva803@gmail.com>
2025-04-21 18:34:53 +0530

    CRUD Opertions

---
+++ src/main/java/REST_API_CRUD_Operations/SbRestServiceApplication.java
@@ -0,0 +1,13 @@
+package REST_API_CRUD_Operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestServiceApplication.class, args);
+	}
+
+}
